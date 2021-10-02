975c509ac1935e4c44c7c8dfe019b2ca0f8762d6
emalianakasmuri <emaliana.kasmuri@gmail.com>
2021-10-02 17:16:44 +0800

    Update RoundingApp
    App is complete

--- samples/src/samples/demo/RoundingApp.java
+++ samples/src/samples/demo/RoundingApp.java
@@ -1,19 +1,28 @@
 package samples.demo;
 
-public class RoundingApp {
+import java.text.DecimalFormat;
 
+public class RoundingApp {
+	
+	/**
+	 * This program calculate progress percentage
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
+		// Dummy data declaration
 		int marks = 73;
 		int total = 150;
-		int progress = (marks/total) * 100;
 		
-		System.out.println("Progress: " + progress);
+		// Calculate percentage
+		double progressRate = (double)marks/total;
+		double progressPencentage =  progressRate * 100.00;
 		
+		// Print result
+		DecimalFormat df = new DecimalFormat("###");		
+		int percentage = Integer.parseInt(df.format(progressPencentage));
+		System.out.println(percentage);
 		
-		
-		
-
 	}
 
 }
