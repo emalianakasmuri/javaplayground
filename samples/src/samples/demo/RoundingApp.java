cb3d63548a1ffa5435d62b3ff9362faf48851f33
emalianakasmuri <emaliana.kasmuri@gmail.com>
2021-10-02 16:43:23 +0800

    No longer required

---
+++ samples/src/samples/demo/RoundingApp.java
@@ -0,0 +1,19 @@
+package samples.demo;
+
+public class RoundingApp {
+
+	public static void main(String[] args) {
+		
+		int marks = 73;
+		int total = 150;
+		int progress = (marks/total) * 100;
+		
+		System.out.println("Progress: " + progress);
+		
+		
+		
+		
+
+	}
+
+}
